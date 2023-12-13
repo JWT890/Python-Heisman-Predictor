8833c600bf2254bf8c90cf2e4124204bb9fa2990
Jon William Taylor <jwtaylor659@gmail.com>
2023-12-13 14:32:59 -0600

    Updated

--- heisman2.py
+++ heisman2.py
@@ -18,9 +18,9 @@ def heisman_predictor(player_stats):
     # Check if each player has enough stats to be considered for the Heisman
     results = []
     for i in range(len(player_stats)):
-        if total_scores[i] >= 1000:
+        if total_scores[i] >= 1500:
             results.append(f"{player_stats[i]['Player']} is this years potential Heisman winner!")
-        if total_scores[i] >= 500:
+        elif total_scores[i] >= 1000:
             results.append(f"{player_stats[i]['Player']} is a strong contender for the Heisman Trophy!")
         else:
             results.append(f"{player_stats[i]['Player']} needs to improve their stats to be considered for the Heisman.")
